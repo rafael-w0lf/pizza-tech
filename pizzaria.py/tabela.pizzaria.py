befc7832a10f01c398b800c3655a32ba1ff632bb
rafael-w0lf <ferreirawolfrafael@gmail.com>
2026-04-05 21:17:24 -0300

    Primeira tabela da Pizza-Tech

---
+++ pizzaria.py/tabela.pizzaria.py
@@ -0,0 +1,42 @@
+import sqlite3
+conexao = sqlite3.connect('pizzaria.db')
+cursor = conexao.cursor()
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS pizzas (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                preco REAL NOT NULL
+               )
+                ''')
+conexao.commit()
+conexao.close()
+
+def adicionar_pizza(nome, preco):
+    conexao = sqlite3.connect('pizzaria.db')
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO pizzas (nome, preco) VALUES (?, ?)", (nome, preco))
+    conexao.commit()
+    conexao.close()
+
+    #pizzas salgadas
+    adicionar_pizza("Mussarela", 39.90)
+    adicionar_pizza("Calabresa", 42.90)
+    adicionar_pizza("Portuguesa", 46.90)
+    adicionar_pizza("Frango com Catupiry", 50.90)
+    adicionar_pizza("Marguerita", 45.90)
+    adicionar_pizza("Quatro Queijos", 52.90)
+    adicionar_pizza("Cane Seca com Queijo", 50.90)
+    adicionar_pizza("Atum com Cebola", 48.90)
+    adicionar_pizza("Pepperoni", 53.90)
+
+    #pizzas doces
+    adicionar_pizza("Chocolate", 45.90)
+    adicionar_pizza("Banana com Canela", 42.90)
+    adicionar_pizza("Romeu e Julieta", 46.90)
+    adicionar_pizza("brigadeiro", 47.90)
+    adicionar_pizza("Nutella", 56.90)
+
+    
+
+    
+
